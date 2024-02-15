60ec250d90eee440559da3bacf9cc0b65f0457e2
tomar2207 <157163413+tomar2207@users.noreply.github.com>
2024-02-15 21:16:32 +0000

    Nothing much

--- docs/script.js
+++ docs/script.js
@@ -6,10 +6,10 @@ function validateForm() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     var checkboxChecked = false;
     if (CC == "") {
-      document.getElementById("ccError").textContent = "Please fill out CC";
+      document.getElementById("ccError").textContent = "\u26A0  Please fill out CC";
     }
     if (subject == "") {
-      document.getElementById("subjectError").textContent = "Please fill out Subject";
+      document.getElementById("subjectError").textContent = "\u26A0  Please fill out Subject";
     }
     for (var i = 0; i < radioButtons.length; i++) {
         if (radioButtons[i].checked) {
@@ -18,7 +18,7 @@ function validateForm() {
         }
     }
     if (!radioSelected) {
-      document.getElementById("radioError").textContent = "Please select Gender";
+      document.getElementById("radioError").textContent = "\u26A0  Please select Gender";
     }
 
     checkboxes.forEach(function(checkbox) {
@@ -27,7 +27,7 @@ function validateForm() {
         }
     });
     if (!checkboxChecked) {
-      document.getElementById("checkboxError").textContent = "Please select atleast one hobby";;
+      document.getElementById("checkboxError").textContent = "\u26A0  Please select atleast one hobby";;
         return false;
     }
   }
